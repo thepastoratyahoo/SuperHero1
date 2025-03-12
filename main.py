4b683075155ea128d4adbe3dd54194951ec1765f
The Pastor At Yahoo! <steve@homesinconnell.com>
2025-03-12 01:28:49 +0000

    Batman
    Fix batman line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 char = input("Enter a character: ")
 type = input("Is your character a superhero: ")
-if type == "yes"or type=="Yes"or type=="Y" or type=="y":
-    print("""Your character is "Marvelous"!""")
+if type == "yes" or type == "Yes" or type == "Y" or type == "y":
+  print("""Your character is "Marvelous"!""")
 else:
-    print("Your character is Boring")
+  print("Your character is Lame")
